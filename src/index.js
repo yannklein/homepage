9f6ae0b86131b15a4b083d584840157a5e800bd7
Yann Klein <yann.klein@me.com>
2020-04-23 20:17:04 +0900

    card finalized

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
-import './fonts/libre-baskerville-v5-latin-regular.woff';
-import './fonts/libre-baskerville-v5-latin-regular.woff2';
-
 import './index.html';
 import './index.scss';
-import './scripts/script';
+
 import { initThree, addBackground, addIntroPopup } from './scripts/initThree';
 import initSideBarFilter from './scripts/sidebar';
 
