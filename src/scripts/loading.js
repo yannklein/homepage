63e35f83b910fd24519d19b5cc24743562bc120d
Yann Klein <yann.klein@me.com>
2020-04-25 19:58:03 +0900

    changes on idex docs

---
+++ src/scripts/loading.js
@@ -0,0 +1,7 @@
+const outroLoading = (doc, loadElement) => {
+  // doc.addEventListener('DOMContentLoaded', () => {
+    loadElement.style.display = 'none';
+  // });
+};
+
+export default outroLoading;
