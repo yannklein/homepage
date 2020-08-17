06290dd6838d1f3eb614cc2fc144b5ed0ab50b56
Yann Klein <yann.klein@me.com>
2020-08-17 17:15:15 +0900

    center card background

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,5 +1,5 @@
 const initSideBarFilter = sidebarLeft => {
-  const cards = document.querySelectorAll('.card');
+  const cards = document.querySelectorAll('.card-frame');
   let currentFilter = 'all';
 
   // Retrieve the icon names (e.g.: .icon-vr => vr)
@@ -19,19 +19,19 @@ const initSideBarFilter = sidebarLeft => {
     iconElmt.addEventListener('click', event => {
       event.preventDefault();
       // Hide all the cards
-      document.querySelector('.card-first').classList.remove('card-first');
+      document.querySelector('.first').classList.remove('first');
       cards.forEach(card => card.style.display = 'none');
       // If the current filter is the one clicked show all
       if (currentFilter === iconName) {
         cards.forEach(card => card.style.display = 'block');
-        cards[0].classList.add('card-first');
+        cards[0].classList.add('first');
         currentFilter = 'all';
       }
       // Only show the targetted cards
       else if (targetCards[0]) {
         targetCards.forEach(card => card.style.display = 'block');
         // Redine the first card of the grid
-        targetCards[0].classList.add('card-first');
+        targetCards[0].classList.add('first');
         // Update the current filter status
         currentFilter = iconName;
       }
