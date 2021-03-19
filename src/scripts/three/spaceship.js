e4887a6dba49ba3065d5d23b0752576718feb50d
Yann Klein <yann.klein@me.com>
2021-03-19 20:04:14 +0900

    Revert "Updates"

---
+++ src/scripts/three/spaceship.js
@@ -0,0 +1,90 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+import starship from '../../images/starship/scene.gltf';
+// starshipBin needed for webpack to take the bin into account
+import starshipBin from '../../images/starship/scene.bin';
+
+import { getCountry, getUTCOffset } from '../locationInfo';
+
+const createSpaceShip = (username, textureLoader, porcelainMat, bgMeshes, world) => {
+  // Load 3D object
+  const ship = new THREE.Group();
+  const loader = new GLTFLoader();
+  loader.load(
+    starship,
+    gltf => {
+      gltf.scene.traverse(o => {
+        if (o.isMesh) {
+          o.material = porcelainMat;
+        }
+      });
+      ship.add(gltf.scene);
+      // Resize/rescale the 3D Object
+      const bbox = new THREE.Box3().setFromObject(gltf.scene);
+      const cent = bbox.getCenter(new THREE.Vector3());
+      const size = bbox.getSize(new THREE.Vector3());
+      // Rescale the object to normalized space
+      const maxAxis = Math.max(size.x, size.y, size.z);
+      gltf.scene.scale.multiplyScalar(0.3 / maxAxis);
+      bbox.setFromObject(gltf.scene);
+      bbox.getCenter(cent);
+      bbox.getSize(size);
+      // Reposition to 0,halfY,0
+      gltf.scene.position.copy(cent).multiplyScalar(-1);
+
+      const geoFlag = new THREE.PlaneGeometry(0.12, 0.08);
+      // Update country according to GH location
+      getCountry(username, fetchedCountry => {
+        textureLoader.load(
+          `https://cdn.staticaly.com/gh/hjnilsson/country-flags/master/svg/${fetchedCountry}.svg`,
+          texFlag => {
+            const matFlag = new THREE.MeshMatcapMaterial({
+              side: THREE.DoubleSide,
+              map: texFlag
+            });
+            const flag = new THREE.Mesh(geoFlag, matFlag);
+            flag.scale.multiplyScalar(0.25);
+            flag.position.z = -0.04;
+            flag.position.x = 0.02;
+            flag.rotation.y = Math.PI / 2;
+            ship.add(flag);
+            const flagBack = new THREE.Mesh(geoFlag, matFlag);
+            flagBack.scale.multiplyScalar(0.25);
+            flagBack.position.z = -0.04;
+            flagBack.position.x = -0.02;
+            flagBack.rotation.y = Math.PI / 2;
+            ship.add(flagBack);
+          }
+        );
+      });
+
+      const speed = 0.0005;
+      let position = 0.11;
+      const distCenter = 1.1;
+      ship.rotation.y = -position * Math.PI - Math.PI / 2;
+      ship.position.x = Math.cos(position * Math.PI) * distCenter;
+      ship.position.z = Math.sin(position * Math.PI) * distCenter;
+      ship.name = "starship";
+      bgMeshes.add(ship);
+
+      const animation = () => {
+        position += speed;
+        ship.position.x = Math.cos(position * Math.PI) * distCenter;
+        ship.position.z = Math.sin(position * Math.PI) * distCenter;
+        ship.position.y = 0.2 * Math.cos(position * Math.PI);
+        ship.rotation.x = -0.2 * Math.cos(position * Math.PI);
+        ship.rotation.z = -0.2 * Math.cos(position * Math.PI - Math.PI / 4);
+        ship.rotation.y = -position * Math.PI;
+      };
+      const bindedAnimation = animation.bind(null, ship);
+      world.animationQueue.push(bindedAnimation);
+    },
+    undefined,
+    e => {
+      console.error(e);
+    }
+  );
+};
+
+export { createSpaceShip };
---
+++ webpack.common.js
@@ -0,0 +1,111 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+    vendor: './src/vendor.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|gltf|glb|bin)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: '/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+              publicPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new WriteFilePlugin(),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'src', 'robots.txt'),
+        to: path.resolve(__dirname, 'docs', 'robots.txt')
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      title: 'yann-home-page',
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: 'head'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'yann-404-page',
+      filename: '404.html',
+      template: './src/404.html',
+      inject: 'head'
+    }),
+    new PreloadWebpackPlugin({
+      rel: 'preload',
+      as(entry) {
+        if (/\.(woff|woff2|ttf|otf)$/.test(entry)) return 'font';
+      },
+      fileWhitelist: [/\.(woffwoff|woff2|ttf|otf)$/],
+      include: 'allAssets'
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    }),
+    new VueLoaderPlugin()
+  ],
+  externals: {
+    $: 'jquery',
+    jquery: 'jQuery',
+    'window.$': 'jquery'
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  }
+};
