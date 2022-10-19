558af9bb8a72ee95b8f60b3d2ef8b7357e2fa08a
Yann Klein <yann.klein@me.com>
2022-10-19 22:44:51 +0900

    switch todays commit from flyio hosting to render hosting

--- src/scripts/github_activity.js
+++ src/scripts/github_activity.js
@@ -1,5 +1,5 @@
 const fetchGithubActivity = (username, day, callback) => {
-  const url = `https://todays-commits.fly.dev/${username}`;
+  const url = `https://todays-commits.onrender.com/${username}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
