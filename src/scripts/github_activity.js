7a00b1aa535d5f33184d56be37fc305c5f0407fc
Yann Klein <yann.klein@me.com>
2023-01-26 15:35:23 +0900

    update todays commit sourfce

--- src/scripts/github_activity.js
+++ src/scripts/github_activity.js
@@ -1,5 +1,5 @@
 const fetchGithubActivity = (username, day, callback) => {
-  const url = `https://todays-commits.onrender.com/${username}`;
+  const url = `https://todays-commits.herokuapp.com/${username}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
