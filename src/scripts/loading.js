90f22330a3c7182955ab81193a7fa41d62ffb238
Yann Klein <yann.klein@me.com>
2024-03-10 16:44:59 +0900

    Updates

--- src/scripts/loading.js
+++
@@ -1,8 +0,0 @@
-const outroLoading = (doc, loadElement) => {
-  const hideLoading = () => {
-    loadElement.style.display = 'none';
-  };
-  doc.addEventListener('DOMContentLoaded', hideLoading);
-};
-
-export default outroLoading;
