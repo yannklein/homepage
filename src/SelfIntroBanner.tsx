cd737326b90e9746446ee01c538ca7dc32a35b2f
Yann Klein <yann.klein@me.com>
2025-09-30 14:59:37 +0200

    refactor SelfIntroBanner styles and enhance markdown rendering options

--- src/SelfIntroBanner.tsx
+++ src/SelfIntroBanner.tsx
@@ -11,12 +11,21 @@ const IntroCard = () => {
     fetch(url)
       .then(response => response.text())
       .then((data) => {
-        setIntroText(data);
+        setIntroText(
+          data
+            // .replace("**Software Developer** · **Bootcamp Manager**", `<p style="text-align: center" >**Software Developer** · **Bootcamp Manager**</p>)
+            .replace("#### Contributions across platforms", "<br/>\n\n")
+            .replace(/---/g, "")
+        );
       });
   }, []);
 
   // Render the markdown
-  const md = new MarkdownIt();
+  const md = new MarkdownIt({
+    html: true,
+    linkify: true,
+    breaks: true
+  });
   const formattedIntro = md.render(introText);
 
   return (
