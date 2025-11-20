5c5055f57a0dd58a2d2fad0999e7c45314cf7c90
Yann Klein <yann.klein@me.com>
2025-11-20 17:53:34 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [],
+  esbuild: {
+    jsx: 'automatic',
+  },
   resolve: {
     extensions: [
       '.gltf',
