5f423047b28d47bba250fe21522d9ae2be914a1f
Yann Klein <yann.klein@me.com>
2025-11-20 21:11:27 +0100

    Updated About.tsx

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
             <meta itemProp="jobTitle" content="Software Developer & Bootcamp Manager" />
             <meta itemProp="worksFor" content="Le Wagon Tokyo" />
             <p className="about-intro" itemProp="description">
-              I’m a maker who loves building things web apps, VR/AR experiments, or AI models that sometimes behave like enthusiastic toddlers. With 15+ years in software engineering, cloud, and machine learning, I’ve led Le Wagon Tokyo’s bootcamps and helped hundreds of students launch tech careers.
+              I’m a maker who loves building things, web apps, VR/AR experiments, or AI models that sometimes behave like enthusiastic toddlers. With 15+ years in software engineering, cloud, and machine learning, I’ve led Le Wagon Tokyo’s bootcamps and helped hundreds of students launch tech careers.
             </p>
             <p itemProp="knowsAbout">
               When I’m not teaching or coding, you’ll find me crafting leather goods, exploring new tech, or riding around discovering new places on wheels.
