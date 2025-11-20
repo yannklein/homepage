6b7c0929c62d653ce512f160ae19a5e7bbf772b3
Yann Klein <yann.klein@me.com>
2025-11-20 19:07:40 +0100

    Updated LogoIcosahedron.tsx

--- src/LogoIcosahedron.tsx
+++ src/LogoIcosahedron.tsx
@@ -18,7 +18,7 @@ const LogoIcosahedron = () => {
     sceneRef.current = scene;
 
     const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 10);
-    camera.position.z = 0.25;
+    camera.position.z = 0.20;
 
     const renderer = new THREE.WebGLRenderer({
       antialias: true,
